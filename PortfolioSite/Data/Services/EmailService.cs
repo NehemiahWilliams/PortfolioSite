c7aac7f2ac84314031e787e7de8d199eec0b008b
Nehemiah Williams <nehemiahwilliams80@gmail.com>
2023-04-12 20:55:10 -0500

    updates email reference to me for forms filled out

--- PortfolioSite/Data/Services/EmailService.cs
+++ PortfolioSite/Data/Services/EmailService.cs
@@ -16,7 +16,7 @@ namespace PortfolioSite.Data.Services
         {
             var client = new SendGridClient(new SendGridClientOptions{ApiKey = EnvironmentVariables.SendGridApiKey, HttpErrorAsException = true });
             var to = new EmailAddress("nwil215@wgu.edu", "Nehemiah");
-            var from = new EmailAddress("nehemiahwilliams80@gmail.com", "Nehemiah");
+            var from = new EmailAddress("nehemiah@madladmiah.dev", "Nehemiah");
             var plainTextContent = body;
             var htmlContent = $"<div> Email: {fromEmail} <br> Name: {fromName} <br> Message: {body}</div>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject + fromName, plainTextContent, htmlContent);
